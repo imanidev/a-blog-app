a5b74d6ff3239c2ddce04f5977ebb1d07c02fb97
Imani Roberts <imaniroberts.dev@gmail.com>
2025-04-01 11:17:54 -0400

    edit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p>Imani Roberts {new Date().getFullYear()}</p>
+      <p>Imani Roberts &copy; {new Date().getFullYear()}</p>
     </footer>
   );
 }
